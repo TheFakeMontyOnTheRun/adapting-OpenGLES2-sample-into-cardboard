8bb9e6df49f3bc2b7938af0c211198a68b67f83b
Daniel Monteiro <danielmonteiro@id.uff.br>
2015-11-28 20:37:27 -0200

    Adding rotation based on Cardboard trigger

--- HelloOpenGLES20/app/src/main/java/com/example/android/opengl/OpenGLES20Activity.java
+++ HelloOpenGLES20/app/src/main/java/com/example/android/opengl/OpenGLES20Activity.java
@@ -23,7 +23,7 @@ import com.google.vrtoolkit.cardboard.CardboardActivity;
 
 public class OpenGLES20Activity extends CardboardActivity {
 
-    private GLSurfaceView mGLView;
+    private MyGLSurfaceView mGLView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -36,21 +36,9 @@ public class OpenGLES20Activity extends CardboardActivity {
     }
 
     @Override
-    protected void onPause() {
-        super.onPause();
-        // The following call pauses the rendering thread.
-        // If your OpenGL application is memory intensive,
-        // you should consider de-allocating objects that
-        // consume significant memory here.
-        mGLView.onPause();
-    }
+    public void onCardboardTrigger() {
+        super.onCardboardTrigger();
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        // The following call resumes a paused rendering thread.
-        // If you de-allocated graphic objects for onPause()
-        // this is a good place to re-allocate them.
-        mGLView.onResume();
+        mGLView.mRenderer.mAngle += 1.0f;
     }
 }
